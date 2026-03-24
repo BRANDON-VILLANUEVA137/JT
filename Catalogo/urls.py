e2820afe4ec689a023178e336ee07a8aabaa2f37
BRANDON_VILLANEVA <brandonvillanueva137@gmail.com>
2026-03-23 19:20:09 -0500

    Crear ProductoURL lista

--- Catalogo/urls.py
+++ Catalogo/urls.py
@@ -5,8 +5,8 @@ app_name = 'catalogo'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('crear/', views.product_create, name='product_create'),
     path('<slug:slug>/editar/', views.product_update, name='product_update'),
     path('<slug:slug>/eliminar/', views.product_delete, name='product_delete'),
+    path('<slug:slug>/', views.product_detail, name='product_detail'),
 ]
