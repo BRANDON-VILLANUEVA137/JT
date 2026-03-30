3311a6461e3cf02a81b793a1f660c2ab98af6238
BRANDON_VILLANEVA <brandonvillanueva137@gmail.com>
2026-03-29 21:24:10 -0500

    Se desabilitó el admin django

--- JUANJO_TECH/urls.py
+++ JUANJO_TECH/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),  # Deshabilitado - usar dashboard custom
     path('', include('Catalogo.urls')),
     path('usuarios/', include('usuarios.urls')),
     path('pedidos/', include('pedidos.urls')),
